df37d91754214c2e8674028236a35a76ec86cb2f
Egor-bog <7548494@gmail.com>
2021-04-05 20:44:42 +0300

    fix: add Meaningful names of identifiers and classes according to rule 3.2 in html-and-css.md

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 //Break things down into smaller steps and take each step at a time.
 
 
+
 // Event handling, user interaction is what starts the code execution.
 
 var taskInput=document.getElementById("new-task");//Add a new task.
